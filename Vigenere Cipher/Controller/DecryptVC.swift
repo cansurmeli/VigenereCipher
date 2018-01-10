cc1803f0616910e1ff4b8511cbbe5390991d279c
Can Sürmeli <c_surmeli@icloud.com>
2018-01-10 13:18:36 +0100

    Constructed the decrpyt UI.

---
+++ Vigenere Cipher/Controller/DecryptVC.swift
@@ -0,0 +1,13 @@
+//
+//  DecryptVC.swift
+//  Vigenere Cipher
+//
+//  Created by Can Sürmeli on 10.01.2018.
+//  Copyright © 2018 Can Sürmeli. All rights reserved.
+//
+
+import UIKit
+
+class DecryptVC: UIViewController {
+	
+}
